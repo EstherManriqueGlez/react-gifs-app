603f6e37ded4e76567bc943cafb7aa8f24f0546e
Esther Manrique <esthermanriqueepam@gmail.com>
2026-01-14 13:39:12 -0600

    Implement search functionality and previous searches feature in GifsApp component

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,11 +1,28 @@
-import { mockGifs } from './mock-data/gifs.mock';
+import { useState } from 'react';
 
+import { mockGifs } from './mock-data/gifs.mock';
 import { CustomHeader } from './shared/components/CustomHeader';
 import { SearchBar } from './shared/components/SearchBar';
 import { PreviousSearches } from './gifs/components/PreviousSearches';
 import { GifList } from './gifs/components/GifList';
 
 export const GifsApp = () => {
+  const [previousTerms, setPreviousTerms] = useState(['dragon ball z']);
+
+  const handleTermClicked = (term: string) => {
+    console.log({ term });
+  };
+
+  const handleSearch = (query: string = '') => {
+    query = query.toLowerCase().trim();
+
+    if (query.length === 0 || previousTerms.includes(query)) return;
+
+    // if (previousTerms.includes(query)) return;
+
+    setPreviousTerms([query, ...previousTerms].splice(0, 8));
+  };
+  
   return (
     <>
       {/* Header */}
@@ -16,10 +33,16 @@ export const GifsApp = () => {
       />
 
       {/* Search */}
-      <SearchBar placeholder="Search for gifs..." />
+      <SearchBar
+        placeholder="Search for gifs..."
+        onQuery={(query: string) => handleSearch(query)}
+      />
 
       {/* Previous searches */}
-      <PreviousSearches searches={['Goku', 'Dragon Ball Z']}/>
+      <PreviousSearches
+        searches={previousTerms}
+        onLabelClicked={handleTermClicked}
+      />
 
       {/* Gifs Grid */}
       <GifList gifs={mockGifs} />
